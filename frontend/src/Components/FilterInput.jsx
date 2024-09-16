38947a83a251f8168c3b0f2d538a0935888d81d5
Dev Bachani <dev22603@gmail.com>
2024-09-17 03:35:50 +0530

    done

--- frontend/src/Components/FilterInput.jsx
+++ frontend/src/Components/FilterInput.jsx
@@ -26,7 +26,8 @@ function FilterInput({ onFilterChange }) {
 			startDate: "",
 			endDate: "",
 			authorName: "",
-			impactFactor: 0,
+			impactFactorMin: 0,
+			impactFactorMax: 0.1,
 		});
 
 		onFilterChange({});
@@ -35,39 +36,40 @@ function FilterInput({ onFilterChange }) {
 	return (
 		<div className="border mt-10 p-4">
 			<form onSubmit={handleSubmit}>
-				<div className="flex flex-wrap space-x-4 justify-center container mx-auto">
-					<div>
-						<label
-							htmlFor="startDate"
-							className="mr-1 font-semibold text-lg"
-						>
-							Start Date:
-						</label>
-						<input
-							type="month"
-							name="startDate"
-							value={filters.startDate}
-							onChange={handleInputChange}
-							className="border p-2 rounded-sm"
-						/>
-					</div>
+				<div className="flex flex-wrap justify-around space-x-4 container items-center">
+					<div className="flex flex-col items-end space-y-1">
+						<div>
+							<label
+								htmlFor="startDate"
+								className="mr-1 font-semibold text-lg"
+							>
+								Start Date:
+							</label>
+							<input
+								type="month"
+								name="startDate"
+								value={filters.startDate}
+								onChange={handleInputChange}
+								className="border p-2 rounded-sm"
+							/>
+						</div>
 
-					<div>
-						<label
-							htmlFor="endDate"
-							className="mr-1 font-semibold text-lg"
-						>
-							End Date:
-						</label>
-						<input
-							type="month"
-							name="endDate"
-							value={filters.endDate}
-							onChange={handleInputChange} 
-							className="border p-2 rounded-sm"
-						/>
+						<div>
+							<label
+								htmlFor="endDate"
+								className="mr-1 font-semibold text-lg"
+							>
+								End Date:
+							</label>
+							<input
+								type="month"
+								name="endDate"
+								value={filters.endDate}
+								onChange={handleInputChange}
+								className="border p-2 rounded-sm"
+							/>
+						</div>
 					</div>
-
 					<div>
 						<label
 							htmlFor="authorName"
@@ -80,46 +82,68 @@ function FilterInput({ onFilterChange }) {
 							name="authorName"
 							placeholder="Author Name"
 							value={filters.authorName}
-							onChange={handleInputChange} 
+							onChange={handleInputChange}
 							className="border p-2 rounded-sm"
 						/>
 					</div>
 
-					<div className="flex justify-between items-center">
+					<div className="flex flex-col justify-between items-center">
 						<label
 							htmlFor="impactFactor"
 							className="mr-1 font-semibold text-lg"
 						>
 							Impact Factor:
 						</label>
-						<input
-							type="range"
-							name="impactFactor"
-							min="0"
-							max="10"
-							step="0.1"
-							value={filters.impactFactor}
-							onChange={handleInputChange} 
-							className="border p-2 rounded-sm max-w-fit"
-						/>
-						<div className="min-w-6 text-center">
-							{filters.impactFactor}
+						<div className="flex flex-col justify-center items-center space-x-2">
+							<input
+								type="number"
+								name="minImpactFactor"
+								min="0"
+								max="10"
+								step="0.1"
+								value={filters.minImpactFactor}
+								onChange={handleInputChange}
+								placeholder="Min"
+								className="border p-2 rounded-sm max-w-fit"
+							/>
+							{/* <span className="font-semibold">to</span> */}
+							<input
+								type="number"
+								name="maxImpactFactor"
+								min="0"
+								max="10"
+								step="0.1"
+								value={filters.maxImpactFactor}
+								onChange={handleInputChange}
+								placeholder="Max"
+								className="border p-2 rounded-sm max-w-fit"
+							/>
 						</div>
 					</div>
+					<div>
+						<label
+							htmlFor="impactFactor"
+							className="mr-1 font-semibold text-lg"
+						>
+							Indexing
+						</label>
 
-					<button
-						type="submit"
-						className="bg-blue-500 text-white p-2"
-					>
-						Submit
-					</button>
-					<button
-						type="button"
-						className="bg-blue-500 text-white p-2"
-						onClick={clearFilter}
-					>
-						Clear Filters
-					</button>
+					</div>
+					<div className="grid row-span-2 space-y-2">
+						<button
+							type="submit"
+							className="bg-blue-500 text-white px-2 max-h-max py-1 w-32  rounded-[4px]"
+						>
+							Submit
+						</button>
+						<button
+							type="button"
+							className="bg-orange-600 text-white px-2 items-center max-h-max py-1 w-32  rounded-[4px]"
+							onClick={clearFilter}
+						>
+							Clear Filters
+						</button>
+					</div>
 				</div>
 			</form>
 		</div>
